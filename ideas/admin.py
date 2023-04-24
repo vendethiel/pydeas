0ae219502b9ccab973b63997bc9c54d80daee98c
vendethiel <vendethiel@hotmail.fr>
2023-04-24 20:47:45 +0200

    Enable django admin for our first 3 models

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Idea, Implementation
+
+admin.site.register(Category)
+admin.site.register(Idea)
+admin.site.register(Implementation)
