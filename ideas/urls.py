8c0b44063ae4973d28f288437fb9f853b31e4a46
vendethiel <vendethiel@hotmail.fr>
2023-04-25 16:28:09 +0200

    Start some views+templates

--- ideas/urls.py
+++ ideas/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'ideas'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.list_categories, name='index'),
+    path('/category/<int:category_id>', views.show_category, name='category'),
+    path('/idea/<int:idea_id>', views.show_idea, name='idea'),
 ]
--- ideas/views.py
+++ ideas/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Idea
 
 
-def index(_request):
-    return HttpResponse("ewe")
+def list_categories(request):
+    categories = Category.objects.all()
+    return render(request, "categories/index.html", {"categories": categories})
+
+
+def show_category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    return render(request, "categories/show.html", {"category": category})
+
+
+def show_idea(request, idea_id):
+    idea = get_object_or_404(Idea, pk=idea_id)
+    return render(request, "ideas/show.html", {"idea": idea})
