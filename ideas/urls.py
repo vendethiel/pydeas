dfbdb9697076a224d337e5740c0076c30ad414c7
vendethiel <vendethiel@hotmail.fr>
2023-05-02 15:30:24 +0900

    Add layout
    Also fix URLs starting with /

--- ideas/urls.py
+++ ideas/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'ideas'
 urlpatterns = [
     path('', views.ListCategoriesView.as_view(), name='index'),
-    path('/category/<int:pk>', views.ShowCategoryView.as_view(), name='category'),
-    path('/idea/<int:pk>', views.ShowIdeaView.as_view(), name='idea'),
+    path('category/<int:pk>', views.ShowCategoryView.as_view(), name='category'),
+    path('idea/<int:pk>', views.ShowIdeaView.as_view(), name='idea'),
 ]
